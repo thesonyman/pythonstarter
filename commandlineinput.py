ae2e0e9cd12f38237f8b440fa709788c412337f9
TriTech <ccoleman.tt@gmail.com>
2014-12-05 09:02:01 -0800

    Update and rename commandlineinput.py to printpi.py

--- commandlineinput.py
+++
@@ -1,2 +0,0 @@
-print ("The total numbers of args passed to the script: %d " % total)
-print ("Args list: %s " % cmdargs)
---
+++ printpi.py
@@ -0,0 +1,2 @@
+print('The value of PI is approximately {}.'.format(math.pi))
+print('The value of PI is approximately {!r}.'.format(math.pi)
